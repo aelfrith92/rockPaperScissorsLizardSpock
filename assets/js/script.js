cffe4be0c249352180b66fb22b290e19f42e77ee
FredM2 <>
2022-01-31 12:42:21 +0000

    Fix function error; Style buttons behaviour and svg images within them

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+	/**
+	* It adds the hovering effect to the svg file, which cannot be styled via CSS
+	*/
+	function hoverButtons (){
+		let buttons = document.getElementsByTagName('button');
+		for(let button of buttons){
+			button.addEventListener('mouseover', function(){
+				this.childNodes[1].childNodes[1].setAttribute('stroke', '#000');
+			});
+			button.addEventListener('mouseleave', function(){
+				this.childNodes[1].childNodes[1].setAttribute('stroke', '#fff');
+			});
+		}
+	}
+	hoverButtons();	
+});
