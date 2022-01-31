d3b5bf6b45ce7a3be55307334870a6d19d63ea55
FredM2 <>
2022-01-31 22:26:53 +0000

    Style the instructions area and adjusted the overall layout within the page; add responsiveness to the instructions area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-	/**
-	* It adds the hovering effect to the svg file, which cannot be styled via CSS
-	*/
-	function hoverButtons (){
-		let buttons = document.getElementsByTagName('button');
-		for(let button of buttons){
-			button.addEventListener('mouseover', function(){
-				this.childNodes[1].childNodes[1].setAttribute('stroke', '#000');
-			});
-			button.addEventListener('mouseleave', function(){
-				this.childNodes[1].childNodes[1].setAttribute('stroke', '#fff');
-			});
-		}
+/**
+* It adds the hovering effect to the svg file, which cannot be styled via CSS
+*/
+function hoverButtons (){
+	let buttons = document.getElementsByTagName('button');
+	for(let button of buttons){
+		button.addEventListener('mouseover', function(){
+			this.childNodes[1].childNodes[1].setAttribute('stroke', '#000');
+		});
+		button.addEventListener('mouseleave', function(){
+			this.childNodes[1].childNodes[1].setAttribute('stroke', '#fff');
+		});
 	}
-	hoverButtons();	
-});
+}
+hoverButtons();
